e7d1a9d1479479c68beb7e069570931bc08e4539
Andrea Gonzalez <andrea.gonz0012@gmail.com>
2022-12-25 15:06:35 -0500

    App Build Step 3: User Interaction

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.sayHelloButton)
+        button.setOnClickListener {
+            Log.v("Hello World","Button clicked!");
+            Toast.makeText(this,"Hello to you too! :D", Toast.LENGTH_SHORT).show();
+        }
     }
 }
