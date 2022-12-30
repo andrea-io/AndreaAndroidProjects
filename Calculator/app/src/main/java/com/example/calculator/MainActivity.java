6f686e1b1054d20f044cd7d133699978502fa286
Andrea Gonzalez <andrea.gonz0012@gmail.com>
2022-12-30 10:51:00 -0500

    Apply operation and checking for operation precedence functions

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +9,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public double applyOperation(char operation, double a, double b) {
+        switch (operation) {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case 'X':
+                return a * b;
+            case '/':
+                if(b == 0) {
+                    throw new UnsupportedOperationException("Cannot divide a number by zero")
+                }
+                return a / b;
+        }
+
+        return 0;
+    }
+
+    public boolean hasPrecedence(char operationOne, char operationTwo) {
+        if((operationOne == 'X' || operationOne == '/') && (operationTwo == '+' || operationTwo == '-')) {
+            return false;
+        }
+
+        return true;
     }
 }
